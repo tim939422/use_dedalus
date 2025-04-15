e55b9e10abe2cc7d71a5c68518948f29cf480b7e
tim939422 <duosifan@hotmail.com>
2025-04-15 14:13:09 +0800

    original channel

---
+++ ivp_2d_channel_flowrate/channel2d.py
@@ -0,0 +1,101 @@
+'''
+created on Apr 1, 2025
+
+@author Duosi Fan
+'''
+
+import numpy as np
+import dedalus.public as d3
+import logging
+logger = logging.getLogger(__name__)
+
+# Parameters
+h = 1.0
+Lx, Ly = np.pi, 2*h
+Nx, Ny = 4, 64
+Ub, Reb = 1.0, 100.0
+dealias = 3/2
+stop_sim_time = 1000
+timestepper = d3.RK222
+max_timestep=0.1
+
+# Basis
+coords =d3.CartesianCoordinates('x','y')
+dist=d3.Distributor(coords,dtype=np.float64)
+xbasis=d3.RealFourier(coords['x'], size=Nx, bounds=(0,Lx), dealias=dealias)
+ybasis=d3.Chebyshev(coords['y'], size=Ny, bounds=(0, Ly), dealias=dealias)
+
+
+# Fields
+p = dist.Field(name='p', bases=(xbasis, ybasis))
+u = dist.VectorField(coords, name='u', bases=(xbasis, ybasis))
+tau_p = dist.Field(name='tau_p') # for pressure gauge
+tau_u1= dist.VectorField(coords, name='tau_u1', bases=xbasis) # no slip BC in continuity equatio div u = 0
+tau_u2= dist.VectorField(coords, name='tau_u2', bases=xbasis) # no-slip BC in momentum equation
+f_dp= dist.Field(name='f_dp') # time-dependent bodyforce to enforce constant flowrate
+
+# substitute
+nu = Ub*h/Reb
+x, y = dist.local_grids(xbasis, ybasis)
+ex, ey = coords.unit_vector_fields(dist)
+lift_basis = ybasis
+lift= lambda A: d3.Lift(A,lift_basis, -1)
+grad_u = d3.grad(u) - ey*lift(tau_u1) # First-order reduction
+
+# problem
+problem=d3.IVP([u,p,tau_p,tau_u1, tau_u2,f_dp], namespace=locals())
+# governing equation: continuity + momentum
+problem.add_equation("trace(grad_u) + tau_p = 0")
+problem.add_equation("dt(u) + grad(p) - nu*div(grad_u)  + lift(tau_u2) + f_dp*ex = -u@grad(u)")
+
+# pressure gauge
+problem.add_equation("integ(p) = 0")
+
+# boundary condition
+problem.add_equation("u(y = 0 ) = 0")
+problem.add_equation("u(y = Ly) = 0")
+
+# fixed flow rate
+problem.add_equation("integ(u@ex) = Lx*Ly")
+
+
+# solver
+solver = problem.build_solver(timestepper)
+solver.stop_sim_time = stop_sim_time
+
+# initial condition
+u['g'][0] = 0
+u['g'][1] = 0
+
+# Analysis
+snapshots = solver.evaluator.add_file_handler('snapshots', sim_dt=1, max_writes=100)
+snapshots.add_task(u, name='velocity')
+snapshots.add_task(p, name='pressure')
+
+checkpoints = solver.evaluator.add_file_handler('checkpoints', sim_dt=100, max_writes=1, mode='overwrite')
+checkpoints.add_tasks(solver.state)
+
+# CFL
+CFL = d3.CFL(solver, initial_dt=max_timestep, cadence=10, safety=0.5, threshold=0.1,
+             max_change=1.5, min_change=0.5, max_dt=max_timestep)
+CFL.add_velocity(u)
+
+# Flow properties
+flow = d3.GlobalFlowProperty(solver, cadence=10) # cadence a.k.a. compute this every cadence step
+flow.add_property(u@ex, name='U')
+
+# Main loop
+try:
+    logger.info('Starting main loop')
+    while solver.proceed:
+        timestep = CFL.compute_timestep()
+        solver.step(timestep)
+        if (solver.iteration-1) % 10 == 0:
+            Uc = flow.max('U')
+            logger.info(f'Iteration={solver.iteration:d}, Time={solver.sim_time:e}, dt={timestep:e}, Uc={Uc:f}')
+except:
+    logger.error('Exception raised, triggering end of main loop.')
+    raise
+finally:
+    solver.log_stats()
+
--- lbvp_1d_channel_flow/channel.py
+++ lbvp_1d_channel_flow/channel.py
@@ -5,38 +5,38 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Parameters
-Ny = 64
+Nz = 64
 dtype = np.float64
 
 # Bases
-coord = d3.Coordinate('y')
-dist = d3.Distributor(coord, dtype=dtype)
-basis = d3.Chebyshev(coord, size=Ny, bounds=(-1, 1))
+zcoord = d3.Coordinate('z')
+dist = d3.Distributor(zcoord, dtype=dtype)
+basis = d3.Chebyshev(zcoord, size=Nz, bounds=(-1, 1))
 
 # Fields
 u = dist.Field(name='u', bases=basis)
-tau_u1 = dist.Field(name='tau_u1') # y = -1
-tau_u2 = dist.Field(name='tau_u2') # y = +1
+tau_u1 = dist.Field(name='tau_u1') # z = -1
+tau_u2 = dist.Field(name='tau_u2') # z = +1
 
 # Substitutions
-y = dist.local_grid(basis)
-dy = lambda A: d3.Differentiate(A, coord)
+z = dist.local_grid(basis)
+dz = lambda A: d3.Differentiate(A, zcoord)
 lift_basis = basis.derivative_basis(2)
 lift = lambda A, n: d3.Lift(A, lift_basis, n)
 
 # Problem
 problem = d3.LBVP([u, tau_u1, tau_u2], namespace=locals())
-problem.add_equation("dy(dy(u)) + lift(tau_u1, -1) + lift(tau_u2, -2) = -2")
-problem.add_equation("u(y=-1) = 0")
-problem.add_equation("u(y=1) = 0")
+problem.add_equation("dz(dz(u)) + lift(tau_u1, -1) + lift(tau_u2, -2) = -2")
+problem.add_equation("u(z=-1) = 0")
+problem.add_equation("u(z=1) = 0")
 
 # Solver
 solver = problem.build_solver()
 solver.solve()
 
 fig, ax = plt.subplots()
-yy = np.linspace(-1, 1, 1001)
-ax.plot(y, u['g'], 'ko', label='Dedalus')
-ax.plot(yy, 1 - yy**2, 'k-', label=r'$u=1-y^2$')
+zz = np.linspace(-1, 1, 1001)
+ax.plot(z, u['g'], 'ko', label='Dedalus')
+ax.plot(zz, 1 - zz**2, 'k-', label=r'$u=1-y^2$')
 ax.legend()
 fig.savefig('channel.png')
